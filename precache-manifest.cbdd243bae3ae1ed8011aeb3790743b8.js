85e0ff1445efbc0c481f6a4ec13b942eede52c40
Alef Pereira <ealefpereira@gmail.com>
2019-11-17 19:14:34 -0400

    Updates

---
+++ precache-manifest.cbdd243bae3ae1ed8011aeb3790743b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d764825152ed5434c8edb9767032c41",
+    "url": "/bullynao/index.html"
+  },
+  {
+    "revision": "8b6efa074deb7cc28c08",
+    "url": "/bullynao/static/css/main.6f84485e.chunk.css"
+  },
+  {
+    "revision": "b7af908c2793841db285",
+    "url": "/bullynao/static/js/2.bf7d14f0.chunk.js"
+  },
+  {
+    "revision": "8b6efa074deb7cc28c08",
+    "url": "/bullynao/static/js/main.ba4e68a0.chunk.js"
+  },
+  {
+    "revision": "3da67effd52b33e82251",
+    "url": "/bullynao/static/js/runtime-main.aba892d4.js"
+  }
+]);
